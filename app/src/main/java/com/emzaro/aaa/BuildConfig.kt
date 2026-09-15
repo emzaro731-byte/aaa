672d64fa56ecbbc21b1dffea43ac75d8e930d391
emzaro731-byte <emzaro731@gmail.com>
2026-09-15 08:49:47 +0100

    feat: add AAA feature configuration

---
+++ app/src/main/java/com/emzaro/aaa/BuildConfig.kt
@@ -0,0 +1,10 @@
+package com.emzaro.aaa
+
+object AAAFeatures {
+    const val CLOUD_BUILD = true
+    const val APK_BUILD = true
+    const val AAB_BUILD = true
+    const val AI_BUILDER = true
+    const val GITHUB_WORKSPACE = true
+    const val LOCAL_PROJECTS = true
+}
